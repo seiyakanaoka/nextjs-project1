eee9cda283c43f4c078e67682dbdf50f4d938707
seiyakanaoka <ttf.1998.seiya@gmail.com>
2022-01-10 15:29:33 +0900

    Project Start

---
+++ components/top/top.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Top: NextPage = () => {
+  return (
+    <>
+      <h1>Welcome to Next.js!</h1>
+    </>
+  )
+};
+
+export default Top;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Top from '@/components/top/top'
+
+const Home: NextPage = () => {
+  return (
+    <Top/>
+  )
+}
+
+export default Home
