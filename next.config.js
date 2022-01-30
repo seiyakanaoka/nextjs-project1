60bb59315e4be2053df313f3d5de6073f37db81a
seiyakanaoka <ttf.1998.seiya@gmail.com>
2022-01-30 21:48:49 +0900

    feat: ログイン機能実装

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  // env: {
-  //   NEXT_PUBLIC_FIREBASE_API_KEY: process.env.FIREBASE_KEY,
-  //   NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_DOMAIN,
-  //   NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-  //   NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-  //   NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_SENDER_ID,
-  //   NEXT_PUBLIC_FIREBASE_APP_ID: process.env.FIREBASE_APPID
-  // }
+  reactStrictMode: true
 }
